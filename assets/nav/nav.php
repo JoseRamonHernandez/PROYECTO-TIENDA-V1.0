9e176b1dbe289375463dbb2cd21e604d39e3cf75
JoseRamonHernandez <67391682+JoseRamonHernandez@users.noreply.github.com / joseramonhernandezmunoz20@gmail.com>
2021-05-03 17:01:43 -0500

    nav/footer

--- assets/nav/nav.php
+++ assets/nav/nav.php
@@ -1,9 +1,8 @@
 
 <nav class="nav nav-pills flex-column flex-sm-row">
-  <a class="flex-sm-fill text-sm-center nav-link active" aria-current="page" href="../../index.php">Inicio</a>
-  <a class="flex-sm-fill text-sm-center nav-link" href="muestraUsuarios.php">Ver Usuarios</a>
-  <a class="flex-sm-fill text-sm-center nav-link" href="create.php">Registrar Usuarios</a>
-  <a class="flex-sm-fill text-sm-center nav-link" href="edit.php">Editar Usuarios</a>
-  <a class="flex-sm-fill text-sm-center nav-link" href="delete.php">Eliminar Usuarios</a>
-  <a class="flex-sm-fill text-sm-center nav-link" href="homeLogin2.php">....</a>
+  <a class="flex-sm-fill text-sm-center nav-link active" aria-current="page" href="adindex.php">Inicio</a>
+  <a class="flex-sm-fill text-sm-center nav-link" href="muestraUsuarios.php">Usuarios</a>
+  <a class="flex-sm-fill text-sm-center nav-link" href="muestraProductos.php">Productos</a>
+  <a class="flex-sm-fill text-sm-center nav-link" href="muestraProovedores.php">Proveedores</a>
+  <a class="flex-sm-fill text-sm-center nav-link" href="home.php">Regresar a Home</a>
 </nav>
--- home.php
+++ home.php
@@ -1,5 +1,5 @@
 <?php
-require_once "nav3.php";
+require_once "./assets/nav/nav3.php";
 ?>
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
@@ -43,5 +43,5 @@ require_once "nav3.php";
 <br>
 <br>
 <?php
-require_once "footer.php";
+require_once "./assets/footer/footer.php";
 ?>
