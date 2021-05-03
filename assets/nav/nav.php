05d6d68ba51eea1a2f1b73e1a2838f56378c64f5
JoseRamonHernandez <67391682+JoseRamonHernandez@users.noreply.github.com / joseramonhernandezmunoz20@gmail.com>
2021-05-03 16:24:04 -0500

    Initial commit

---
+++ assets/nav/nav.php
@@ -0,0 +1,9 @@
+
+<nav class="nav nav-pills flex-column flex-sm-row">
+  <a class="flex-sm-fill text-sm-center nav-link active" aria-current="page" href="../../index.php">Inicio</a>
+  <a class="flex-sm-fill text-sm-center nav-link" href="muestraUsuarios.php">Ver Usuarios</a>
+  <a class="flex-sm-fill text-sm-center nav-link" href="create.php">Registrar Usuarios</a>
+  <a class="flex-sm-fill text-sm-center nav-link" href="edit.php">Editar Usuarios</a>
+  <a class="flex-sm-fill text-sm-center nav-link" href="delete.php">Eliminar Usuarios</a>
+  <a class="flex-sm-fill text-sm-center nav-link" href="homeLogin2.php">....</a>
+</nav>
